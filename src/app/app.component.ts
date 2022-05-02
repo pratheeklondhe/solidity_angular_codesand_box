f541b40781b0b72b401f61b237e3390fb8026c74
Pratheek Londhe <41989079+pratheeklondhe@users.noreply.github.com>
2022-05-02 19:04:12 +0530

    web3 init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Web3 } from "../web3";
 
 @Component({
   selector: "app-root",
@@ -9,6 +10,6 @@ export class AppComponent {
   title = "CodeSandbox";
 
   ngOnInit() {
-    console.log((window as any).web3);
+    console.log("hiiii ", Web3);
   }
 }
